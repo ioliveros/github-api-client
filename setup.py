2bbc832af0a7e744958e4741b9f6419cdabc4eb0
ioliveros <herotayo@gmail.com>
2020-10-20 10:09:21 +0000

    changes in test and structure, added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='github_api_client',
+	version='1.1',
+	author='Ian Oliveros',
+	author_email='ioliveros.dev@gmail.com',
+	packages=find_packages(),
+	python_requires='>=3.6'
+)
